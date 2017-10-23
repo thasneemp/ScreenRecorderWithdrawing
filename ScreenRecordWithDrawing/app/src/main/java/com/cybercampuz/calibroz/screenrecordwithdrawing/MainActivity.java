d724f1f66b33a06b983c0214b5be856524617dac
muhammed <muhammed.thasneem@yahoo.com>
2017-10-23 13:29:07 +0530

    final code

--- ScreenRecordWithDrawing/app/src/main/java/com/cybercampuz/calibroz/screenrecordwithdrawing/MainActivity.java
+++ ScreenRecordWithDrawing/app/src/main/java/com/cybercampuz/calibroz/screenrecordwithdrawing/MainActivity.java
@@ -1,13 +1,89 @@
 package com.cybercampuz.calibroz.screenrecordwithdrawing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.mukesh.DrawingView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private DrawingView mDrawingView;
+    private MediaProjectionManager mProjectionManager;
+    private Button mRecordButton;
+    private MediaProjection mMediaProjection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mRecordButton = (Button) findViewById(R.id.recordButton);
+        mProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+
+        mDrawingView = (DrawingView) findViewById(R.id.scratch_pad);
+
+        mDrawingView.initializePen();
+
+        mRecordButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (mMediaProjection == null && !isServiceRunning(RecorderService.class)) {
+            //Request Screen recording permission
+            startActivityForResult(mProjectionManager.createScreenCaptureIntent(), Const.SCREEN_RECORD_REQUEST_CODE);
+        } else if (isServiceRunning(RecorderService.class)) {
+            //stop recording if the service is already active and recording
+            Toast.makeText(MainActivity.this, "Screen already recording", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean isServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+
+        //Result for system windows permission required to show floating controls
+        if (requestCode == Const.SYSTEM_WINDOWS_CODE) {
+            return;
+        }
+
+        //The user has denied permission for screen mirroring. Let's notify the user
+        if (resultCode == RESULT_CANCELED && requestCode == Const.SCREEN_RECORD_REQUEST_CODE) {
+            Toast.makeText(this,
+                    getString(R.string.screen_recording_permission_denied), Toast.LENGTH_SHORT).show();
+            //Return to home screen if the app was started from app shortcut
+            if (getIntent().getAction().equals(getString(R.string.app_shortcut_action)))
+                this.finish();
+            return;
+
+        }
+
+        /*If code reaches this point, congratulations! The user has granted screen mirroring permission
+        * Let us set the recorderservice intent with relevant data and start service*/
+        Intent recorderService = new Intent(this, RecorderService.class);
+        recorderService.setAction(Const.SCREEN_RECORDING_START);
+        recorderService.putExtra(Const.RECORDER_INTENT_DATA, data);
+        recorderService.putExtra(Const.RECORDER_INTENT_RESULT, resultCode);
+        startService(recorderService);
+
     }
 }
